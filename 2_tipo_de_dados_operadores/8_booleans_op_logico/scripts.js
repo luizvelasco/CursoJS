1304df7174fe5ad6c134aa207a00693cf8dc83fa
Luiz Velasco <luizvelasco@gmail.com>
2023-07-07 17:37:21 -0300

    op lógicos

---
+++ 2_tipo_de_dados_operadores/8_booleans_op_logico/scripts.js
@@ -0,0 +1,10 @@
+console.log(5 > 3 && 3 == 2); //false
+console.log(5 > 3 || 3 == 1); //true
+
+console.log(3 == 3 && "Velasco" == "Velasco"); //true
+console.log("Felipe" == "João" || false); //false;
+
+console.log(!(!(true && true))); //true
+
+console.log(true && true); //true
+console.log(false || false); //false
