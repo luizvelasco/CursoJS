7f2231c7b9bdd759f8aaf08f51d67a5b8fdb26c7
luizvelasco <luizvelasco@gmail.com>
2023-07-09 14:15:02 -0300

    exerc

---
+++ 03_exercicios_tipo_dados/exercicio_03/scripts.js
@@ -0,0 +1,3 @@
+console.log(5 > 3);
+console.log(5 <= 5);
+console.log("Velasco" != "Velascoo");
---
+++ 03_exercicios_tipo_dados/exercicio_04/scripts.js
@@ -0,0 +1,3 @@
+console.log((5 > 3) && (10 > 5));
+console.log((1 > 2) || (5 < 10));
+console.log(!false);
