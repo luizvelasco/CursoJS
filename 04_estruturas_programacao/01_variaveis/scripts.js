8407297c53d125d63cb0f22aa97f6c8168325a7b
luizvelasco <luizvelasco@gmail.com>
2023-07-09 15:37:02 -0300

    variaveis

---
+++ 04_estruturas_programacao/01_variaveis/scripts.js
@@ -0,0 +1,19 @@
+let nome = "Velasco";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "João";
+console.log(nome);
+
+laranjas = 654564;
+
+console.log(laranjas);
+
+laranjas = "Laranja";
+
+console.log(laranjas);
---
+++ 04_estruturas_programacao/02_/scripts.js
@@ -0,0 +1,8 @@
+let nome = "Velasco";
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
