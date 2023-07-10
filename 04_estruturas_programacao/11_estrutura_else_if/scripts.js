399cd252e615ad076175ad073a43669cb18dd929
Luiz Velasco <luizvelasco@gmail.com>
2023-07-10 19:03:32 -0300

    else if

---
+++ 04_estruturas_programacao/11_estrutura_else_if/scripts.js
@@ -0,0 +1,16 @@
+let nome = "Velasco";
+let idade = 30;
+
+if (nome != undefined && nome == "Joaquim"){
+    console.log("O nome está definido");
+} else if (nome == "Velasco" && nome.length > 5 && idade == 50){
+    console.log("O nome é Velasco");
+} else {
+    console.log("Não é Velasco");
+}
+
+if (1 > 2) {
+    console.log("Teste");
+} else if (1 == 1) {
+    console.log("testando");
+}
