9bb5e4fb687a78426e09f18f930181c49b661d3f
Luiz Velasco <luizvelasco@gmail.com>
2023-07-10 13:56:30 -0300

    math

---
+++ 04_estruturas_programacao/07_funcao_math/scripts.js
@@ -0,0 +1,15 @@
+let maior = Math.max(6,12,28,55,8);
+
+console.log(maior);
+
+let menor = Math.min(6,12,28,55,8);
+
+console.log(menor);
+
+let arredondar = Math.round(5.29654654);
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.264564564);
+
+console.log(arredondarParaCima);
---
+++ 04_estruturas_programacao/08_funcao_console.log/scripts.js
@@ -0,0 +1,7 @@
+let idade = 30;
+let nome = "velasco";
+
+console.log(idade);
+console.log(nome);
+
+console.log(`O meu nome é ${nome}, e tenho ${idade} anos`);
