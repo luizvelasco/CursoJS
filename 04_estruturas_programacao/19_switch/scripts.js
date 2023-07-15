73583f7185b3e3a052ee58b5813c39395b19df4f
luizvelasco <luizvelasco@gmail.com>
2023-07-15 12:14:25 -0300

    switch

---
+++ 04_estruturas_programacao/19_switch/scripts.js
@@ -0,0 +1,13 @@
+let nome = "João";
+
+switch(nome) {
+    case "Velasco":
+        console.log("O nome é Velasco");
+        break;
+    case "João": 
+        console.log("O nome é João");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+}
