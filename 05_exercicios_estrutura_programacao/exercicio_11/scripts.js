0d37772be375cecf719b8641f7d90fd78cd50fc4
Luiz Velasco <luizvelasco@gmail.com>
2023-07-18 19:53:17 -0300

    exer

---
+++ 05_exercicios_estrutura_programacao/exercicio_11/scripts.js
@@ -0,0 +1,10 @@
+let idade = 19;
+let cnh = false;
+
+if (idade >= 18 && cnh == true){
+    console.log("Sim, pode dirigir");
+} else if (idade >= 18 && cnh == false) {
+    console.log("tem mais de 18 mas não tem cnh");
+} else {
+    console.log("Não tem 18 nem cnh");
+}
