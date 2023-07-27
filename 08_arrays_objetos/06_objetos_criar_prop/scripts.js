9088a63a935e95e5397eaf05246f764edbb8ebd9
Luiz Velasco <luizvelasco@gmail.com>
2023-07-27 19:31:57 -0300

    Criar propriedades para o objeto

---
+++ 08_arrays_objetos/06_objetos_criar_prop/scripts.js
@@ -0,0 +1,15 @@
+let pessoa = {
+    nome: "Velasco",
+    idade: 37,
+    profissao: "Militar"
+}
+
+console.log(pessoa.nome);
+
+delete pessoa.nome;
+
+console.log(pessoa.nome);
+
+pessoa.casado = false;
+
+console.log(pessoa.casado);
