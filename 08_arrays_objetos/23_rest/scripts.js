31586cfc07946da557e936bb086254eb57dffff0
luizvelasco <luizvelasco@gmail.com>
2023-07-29 15:54:42 -0300

    rest e destruction

---
+++ 08_arrays_objetos/23_rest/scripts.js
@@ -0,0 +1,12 @@
+let num = 1;
+let num1 = 5;
+let num2 = 3;
+let num3 = 4;
+
+function imprimirNumeros(...args) {
+    for (let i = 0; i < args.length; i++){
+        console.log(args[i]);
+    }
+}
+
+imprimirNumeros(num, num1, num2);
