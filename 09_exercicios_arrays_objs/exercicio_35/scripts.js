8400fb31d3aaeb3191f713517bdcce53e13446cb
Luiz Velasco <luizvelasco@gmail.com>
2023-07-31 19:35:42 -0300

    exercs

---
+++ 09_exercicios_arrays_objs/exercicio_35/scripts.js
@@ -0,0 +1,19 @@
+let calculardora = {
+    somar: function(a, b){
+        console.log(a + b);
+    },
+    subtrair: function(a, b){
+        console.log(a - b);
+    },
+    multipilicar: function(a, b){
+        console.log(a * b);
+    },
+    dividir: function(a, b){
+        console.log(a / b);
+    }
+};
+
+calculardora.somar(2,6);
+calculardora.subtrair(10,6);
+calculardora.multipilicar(2,3);
+calculardora.dividir(9,3);
