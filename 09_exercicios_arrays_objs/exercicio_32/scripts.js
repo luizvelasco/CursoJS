a15997e48d5fe8ed4a9cd47193cb53d8fae39afc
Luiz Velasco <luizvelasco@gmail.com>
2023-07-31 19:29:30 -0300

    exer

---
+++ 09_exercicios_arrays_objs/exercicio_32/scripts.js
@@ -0,0 +1,5 @@
+let nomes = ["luiz", "antonio", "braga", "velasco"];
+
+for (i = 0; i < nomes.length; i++) {
+    console.log(nomes[i]);
+}
---
+++ 09_exercicios_arrays_objs/exercicio_33/scripts.js
@@ -0,0 +1,9 @@
+let pessoa = {
+    "nome": "Velasco",
+    "idade": 37,
+    "profissao": "militar"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+console.log(pessoa.profissao);
