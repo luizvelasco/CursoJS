555cbe8a9c05ebe416c66dd9a3d8a729064ba93a
luizvelasco <luizvelasco@gmail.com>
2023-08-14 08:36:35 -0300

    aulas

---
+++ LIVRO/CAP03/exer_01/script.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = '20';
+
+console.log(x + y); //concatena
+
+y = Number(y); //converte pra número
+
+console.log(x + y); //soma
---
+++ LIVRO/CAP03/exer_02/script.js
@@ -0,0 +1,15 @@
+let a = 15;
+let b = 5;
+let c;
+
+c = a + b;
+console.log(c); //20
+
+c = a - b;
+console.log(c); //10
+
+c = a * b;
+console.log(c); //75
+
+c = a / b;
+console.log(c); //3
