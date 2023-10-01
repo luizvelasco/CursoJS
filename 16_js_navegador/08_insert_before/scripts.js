e87953bddecc60f3bc8215ea70a0cbbe443f9450
luizvelasco <luizvelasco@gmail.com>
2023-10-01 15:03:30 -0300

    insert before

---
+++ 16_js_navegador/08_insert_before/scripts.js
@@ -0,0 +1,5 @@
+let novoElemento = document.createElement("p");
+let elementoAlvo = document.querySelector("#titulo-principal");
+let elementoPai = document.querySelector("#container-principal");
+
+elementoPai.insertBefore(novoElemento, elementoAlvo);
