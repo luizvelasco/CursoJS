a9c0bf2e418d2451c184cb9ab823721b51019053
Luiz Velasco <luizvelasco@gmail.com>
2023-10-10 19:48:11 -0300

    dom

--- 16_js_navegador/04_by_tag/scripts.js
+++ 16_js_navegador/04_by_tag/scripts.js
@@ -2,4 +2,4 @@ console.log(document.getElementsByTagName('h1'));
 
 console.log(document.getElementsByTagName('p'));
 
-console.log(document.getElementsByTagName('li'));
+// console.log(document.getElementsByTagName('li'));
