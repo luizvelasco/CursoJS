c9883eabfaf86b99c8b06fac88de1ac198821528
luizvelasco <luizvelasco@gmail.com>
2023-10-12 20:13:31 -0300

    queryselector

--- 16_js_navegador/06_by_class/scripts.js
+++ 16_js_navegador/06_by_class/scripts.js
@@ -1,5 +1,3 @@
-console.log(document.getElementsByTagName('li'));
-
 console.log(document.getElementsByClassName('itens-azuis'));
 
 console.log(document.getElementsByClassName('itens-vermelhos'));
--- 16_js_navegador/07_by_query/scripts.js
+++ 16_js_navegador/07_by_query/scripts.js
@@ -4,6 +4,4 @@ console.log(document.querySelector('#segundo-container h1'));
 
 console.log(document.querySelector('div div p'));
 
-console.log(document.querySelector('footer h2'));
-
 console.log(document.querySelector('ul .itens-azuis'));
