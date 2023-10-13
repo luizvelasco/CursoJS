c68a874ff8be1b8f9c12d7b524c60e009ebe6365
luizvelasco <luizvelasco@gmail.com>
2023-10-13 08:38:19 -0300

    nodes

--- 16_js_navegador/08_insert_before/scripts.js
+++ 16_js_navegador/08_insert_before/scripts.js
@@ -1,4 +1,6 @@
 let novoElemento = document.createElement("p");
+let texto = document.createTextNode("Algum texto");
+novoElemento.appendChild(texto);
 let elementoAlvo = document.querySelector("#titulo-principal");
 let elementoPai = document.querySelector("#container-principal");
 
