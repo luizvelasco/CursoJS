c1515b1fa4bedf242c389fedfe928a93f04028fe
luizvelasco <luizvelasco@gmail.com>
2023-10-20 08:36:12 -0300

    writefile

--- 19_node/03_file_system/index.js
+++ 19_node/03_file_system/index.js
@@ -1,4 +1,4 @@
-let {readFile} = require('fs');
+let {readFile, writeFile} = require('fs');
 
 readFile("arquivo.txt", "utf8", (error, texto) => {
     if (error) {
@@ -7,3 +7,11 @@ readFile("arquivo.txt", "utf8", (error, texto) => {
         console.log(texto);
     }
 });
+
+writeFile ("arquivo.txt", "Texto por writeFile", (error) => {
+    if (error) {
+        throw error;
+    } else {
+        console.log("Escreveu com sucesso");
+    }
+})
