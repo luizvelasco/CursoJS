655e57214c8955b98483df10dccd8a10b62edb1d
luizvelasco <luizvelasco@gmail.com>
2023-10-30 09:11:29 -0300

    classes

---
+++ 22_ES6/08_classes/scripts.js
@@ -0,0 +1,21 @@
+class Product {
+    constructor (name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    productWithDiscount (discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product("Camisa Gola V", 20);
+
+console.log(shirt.name);
+
+console.log(shirt.productWithDiscount(10));
+console.log(shirt.productWithDiscount(50));
+
+const tenis = new Product("Tenis Verde", 120);
+
+console.log(tenis.productWithDiscount(20));
